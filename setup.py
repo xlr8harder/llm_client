fcbbf0dfafc52b1c549b17c1fe0aa54f40fd3f10
xlr8 <xlr8harder@users.noreply.github.com>
2025-06-27 20:34:01 +0300

    increment version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="llm_client",
-    version="0.1.1",
+    version="0.1.2",
     author="",
     author_email="",
     description="A clean, modular client library for interacting with various LLM providers",
